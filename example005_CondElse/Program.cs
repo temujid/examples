5c81b33b5fcaa1c66a9c6d3a5c3f9e4e7bb714b7
Timur <temuchin@ya.ru>
2022-09-21 22:26:41 +0400

    ifelse

---
+++ example005_CondElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Type your name");
+string username = Console.ReadLine();
+
+if (username == Тим)
+{
+    Console.WriteLine("Ура! Это же Тим!");
+}
+else 
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
